4adf9c75c11cdb1c85e86f8c32072e91cafa90b2
christophe <christophe@gmail.com>
2023-12-23 13:12:52 -0500

    Build and push test

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 import { AppService } from './app.service';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule);
-  const appService = app.get(AppService)
-  console.log(appService.getHello());
+  // const app = await NestFactory.createApplicationContext(AppModule);
+  // const appService = app.get(AppService)
+  // console.log(appService.getHello());
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
